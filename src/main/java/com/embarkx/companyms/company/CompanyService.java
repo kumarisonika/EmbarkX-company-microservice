04f58b07d1c4ab5a1deabca75c9fb03bb30c849b
Kumari Sonika <kumarisonika1929@gmail.com>
2026-02-06 00:03:18 +0530

    rabbitmq, configuration

--- src/main/java/com/embarkx/companyms/company/CompanyService.java
+++ src/main/java/com/embarkx/companyms/company/CompanyService.java
@@ -1,5 +1,8 @@
 package com.embarkx.companyms.company;
 
+import com.embarkx.companyms.company.clients.ReviewClient;
+import com.embarkx.companyms.company.dto.ReviewMessage;
+import jakarta.ws.rs.NotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -8,9 +11,11 @@ import java.util.List;
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
+    private ReviewClient reviewClient;
 
-    public CompanyService(CompanyRepository companyRepository) {
+    public CompanyService(CompanyRepository companyRepository, ReviewClient reviewClient) {
         this.companyRepository = companyRepository;
+        this.reviewClient = reviewClient;
     }
 
     public List<Company> findAll(){
@@ -40,6 +45,15 @@ public class CompanyService {
         existingCompany.setDescription(updatedCompany.getDescription());
 
         return companyRepository.save(existingCompany);
+    }
+
+    public void updateCompanyRating(ReviewMessage reviewMessage){
+        System.out.println(reviewMessage.getDescription());
+        Company company =  companyRepository.findById(reviewMessage.getCompanyId())
+                .orElseThrow(()-> new NotFoundException("Company not found"+ reviewMessage.getCompanyId()));
+        double averageRating = reviewClient.getAverageRatingForCompany(reviewMessage.getCompanyId());
+        company.setRating(averageRating);
+        companyRepository.save(company);
     }
 
 
